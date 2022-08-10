940c4dcab58944003364a3bce93415bf8e34e318
Doug A <douganderson444@gmail.com>
2022-08-09 22:25:37 -0300

    add calcDiff between canvas and link coords 💠

--- src/lib/Links.svelte
+++ src/lib/Links.svelte
@@ -7,9 +7,10 @@
 	import { link, curveBumpX } from 'd3-shape';
 
 	export let links;
+	export let canvas;
 
 	export let strokeColor = 'green';
-	export let strokeWidth = 3;
+	export let strokeWidth = 18;
 	export let arrowColor = 'green';
 
 	export let strokeOpacity = '0.5';
@@ -30,15 +31,39 @@
 		mounted = true;
 	});
 
+	function calcDiff(child) {
+		if (child == canvas) return { x: child.offsetLeft, y: child.offsetTop };
+		// need to get the offetTop from canvas, which may be different from the sourceEl offetTop
+		// get different between bounding rect top between sourceEl and canvas
+		let sourceOffsetTop = child.getBoundingClientRect().top;
+		let canvasOffsetTop = canvas.getBoundingClientRect().top;
+		let sourceOffsetTopDiff = sourceOffsetTop - canvasOffsetTop; // zero if same
+		let sourceOffsetTopDiffPx = sourceOffsetTopDiff * window.devicePixelRatio;
+
+		// same for left
+		let sourceOffsetLeft = child.getBoundingClientRect().left;
+		let canvasOffsetLeft = canvas.getBoundingClientRect().left;
+		let sourceOffsetLeftDiff = sourceOffsetLeft - canvasOffsetLeft; // zero if same
+		let sourceOffsetLeftDiffPx = sourceOffsetLeftDiff * window.devicePixelRatio;
+
+		return { x: sourceOffsetLeftDiff, y: sourceOffsetTopDiff };
+	}
+
 	function genPath(link) {
 		let sourceEl = document.getElementById(link.source.id);
 		let targetEl = document.getElementById(link.target.id);
 
 		// centered
-		sourceX = sourceEl?.offsetLeft + sourceEl.offsetWidth / 2;
-		sourceY = sourceEl?.offsetTop + sourceEl.offsetHeight / 2;
-		targetX = targetEl?.offsetLeft + targetEl.offsetWidth / 2;
-		targetY = targetEl?.offsetTop + targetEl.offsetHeight / 2;
+		const { x: sx, y: sy } = calcDiff(sourceEl);
+		const { x: tx, y: ty } = calcDiff(targetEl);
+
+		/**
+		 * Not sure why the svgs are slightly off by 3 pixels, but this is a workaround
+		 */
+		sourceX = sx + sourceEl.offsetWidth / 2 - 3;
+		sourceY = sy + sourceEl.offsetHeight / 2 - 3;
+		targetX = tx + targetEl.offsetWidth / 2 - 3;
+		targetY = ty + targetEl.offsetHeight / 2 - 3;
 
 		let d = generateXcurve({
 			source: [sourceX, sourceY],
