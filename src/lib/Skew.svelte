0c52998ee0ef9568eb011dec6ea89ea919d30023
Doug A <douganderson444@gmail.com>
2022-08-10 12:07:26 -0300

    add Skew styling Comp

---
+++ src/lib/Skew.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let color: string = 'pink';
+
+	let green = `before:bg-green-500/50 text-black `;
+	let pink = 'before:bg-pink-500 text-white';
+</script>
+
+<span
+	class="{color == 'pink'
+		? pink
+		: green} before:inline-block before:absolute before:-inset-1 before:-skew-y-3 relative inline-block"
+	><span class="relative pr-1"><slot /></span>
+</span>
