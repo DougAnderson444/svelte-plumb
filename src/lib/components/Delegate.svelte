99557a28910d84decdecc8a068a72de5e209f95b
Doug A <douganderson444@gmail.com>
2023-01-28 12:21:00 -0400

    refactor to emit events

--- src/lib/components/Delegate.svelte
+++ src/lib/components/Delegate.svelte
@@ -4,14 +4,13 @@
 	// @ts-nocheck
 
 	// The delegated start point component is on the top right hand side of the parent element
-	import { onMount, onDestroy, createEventDispatcher } from 'svelte';
-	import { DROPZONE } from '$lib/constants.js';
+	import { createEventDispatcher } from 'svelte';
 
-	export let sx = 0; // starting x
-	export let sy = 0;
+	export let sx; // starting x
+	export let sy;
 	// export let ex = 0; // ending x
 	// export let ey = 0;
-	export let as = 0; // angle start
+	export let as; // angle start
 
 	export let mounted = false;
 	export let show = true; // boolean toggle whether to show the resizer handle or not
@@ -23,21 +22,22 @@
 	let x = 0;
 	let y = 0;
 
-	$: if (mounted && handle && handle?.offsetWidth) {
-		// console.log(handle?.parentNode, `[data-${DROPZONE}]`, handle?.closest(`[data-${DROPZONE}]`));
-		x = -handle?.offsetWidth; // initialize position to far right corner
+	$: if (mounted && handle && handle?.parentNode?.offsetWidth) {
+		x = handle?.parentNode.offsetWidth; // initialize position to far right corner
 		dispatch('ready', { handle }); // let the parent know we're ready to track
 	}
 
 	$: y = handle && as > 0 ? -handle?.offsetHeight : 0; // above if on top
-	$: x = handle && (!!sy || !!sx) && as < 0.6 && as > -0.6 ? 0 : -handle?.offsetWidth;
+	$: if (handle && !!as) {
+		x = as < 0.6 && as > -0.6 ? -handle?.offsetWidth : 0;
+	}
 </script>
 
 {#if show}
 	<div
 		bind:this={handle}
 		class="cursor-pointer select-none font-mono p-1 text-neutral-400 text-sm bg-white/50 z-50"
-		style="position: absolute; right:{x}px; top:{y}px;"
+		style="position: absolute; left:{x}px; top:{y}px;"
 	>
 		<slot
 			>Connect
