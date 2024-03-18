f082b75cf90ac4d75f63773b418ae919137a2e80
Doug Anderson444 <douganderson444@gmail.com>
2024-03-18 10:10:34 -0300

    replace svelte-preprocess with vitePreprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
-
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({
-		// postcss: true,
-		sourceMap: process.env.NODE_ENV !== 'development',
-		postcss: {
-			configFilePath: path.resolve(__dirname, './postcss.config.js')
-			// ,
-			// prependData:
-			// 	process.env.NODE_ENV !== 'development'
-			// 		? `@import '${path.resolve('./src/utilities.css')}';`
-			// 		: ''
-		}
-	}),
-
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
 			pages: 'docs',
